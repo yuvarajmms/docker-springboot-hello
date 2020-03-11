2822f87410888de50782b972e5df1b30122857aa
yuvarajmms <31970667+yuvarajmms@users.noreply.github.com>
2020-03-11 22:08:21 +0530

    Change

--- src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello World\n";
+        return "Hello World from Controller \n";
     }
     
 }
